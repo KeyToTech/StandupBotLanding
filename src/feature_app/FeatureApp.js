d7f9203ae4180fd5da10d47ca1494740ab761fcf
OlehBabiy <luisowen11@gmail.com>
2019-03-12 11:38:38 +0200

    1) Made a layout structure. 2) Added a method Map. 3) Added icons.

---
+++ src/feature_app/FeatureApp.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import '../feature_app/feature.css';
+
+const wrapContent = [
+
+    {
+        img: "",
+        text: "Easy Customization",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    },
+
+    {
+        img: "Super Fast",
+        text: "",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    },
+
+    {
+        img: "",
+        text: "Cloud Upload",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    },
+
+    {
+        img: "",
+        text: "Multi Control",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    },
+
+    {
+        img: "",
+        text: "Fast Integrations",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    },
+
+    {
+        img: "",
+        text: "100% Secure",
+        description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
+    }
+]
+
+const col = wrapContent.map((table) =>
+
+    <div className="col-app">
+        <img className="img-fa" />
+        <div className="container-ct-ca">
+            <div className="col-tx-f">{table.text}</div>
+            <div className="col-desc-f">{table.description}</div>
+        </div>
+    </div>
+);
+
+const FeatureApp = () => (
+    <div className="feature-bar">
+        <div className="background-feature"></div>
+        <div className="wrap-feature-app">
+            <div className="container-context-app">
+                <div className="tx-f-a">Why everybody choose this app</div>
+                <div className="desct-f-a">Innovative solutions with the best.  Incididunt dolor sit amet, 
+                consectetur adipiscing elitsed tempor  vel metus scelerisque ante sollicitudin. </div>
+            </div>
+            <div className="container-features">
+                {col}
+            </div>
+        </div>
+    </div>
+)
+
+export default FeatureApp;
