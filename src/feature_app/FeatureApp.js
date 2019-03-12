906c0cce5e72db9bf4be49fae25324a2fc4aa68c
OlehBabiy <luisowen11@gmail.com>
2019-03-12 13:09:32 +0200

    1) added icons. 2) added style.

--- src/feature_app/FeatureApp.js
+++ src/feature_app/FeatureApp.js
@@ -1,41 +1,47 @@
 import React, { Component } from 'react';
 import '../feature_app/feature.css';
 import bg from '../image/feature-app/bg-feature-app.png';
+import icon1 from '../image/feature-app/icon-f2-1.png';
+import icon2 from '../image/feature-app/icon-f2-2.png';
+import icon3 from '../image/feature-app/icon-f2-3.png';
+import icon4 from '../image/feature-app/icon-f2-4.png';
+import icon5 from '../image/feature-app/icon-f2-5.png';
+import icon6 from '../image/feature-app/icon-f2-6.png';
 
 const wrapContent = [
 
     {
-        img: "",
+        img: icon1,
         text: "Easy Customization",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     },
 
     {
-        img: "",
+        img: icon2,
         text: "Super Fast",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     },
 
     {
-        img: "",
+        img: icon3,
         text: "Cloud Upload",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     },
 
     {
-        img: "",
+        img: icon4,
         text: "Multi Control",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     },
 
     {
-        img: "",
+        img: icon5,
         text: "Fast Integrations",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     },
 
     {
-        img: "",
+        img: icon6,
         text: "100% Secure",
         description: "Innovative solutions with the best.  Incididunt dolor sit amet consec tetur adipiscing elit."
     }
@@ -44,7 +50,7 @@ const wrapContent = [
 const col = wrapContent.map((table) =>
 
     <div className="col-app">
-        <img className="img-fa" />
+        <img className="img-fa" src={table.img} alt="icon" />
         <div className="container-ct-ca">
             <div className="col-tx-f">{table.text}</div>
             <div className="col-desc-f">{table.description}</div>
