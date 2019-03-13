b6f8f66ddb693d5800231e3ebb122680d2d2c369
OlehBabiy <luisowen11@gmail.com>
2019-03-13 12:00:24 +0200

    Added line decor.

--- src/testimonial/Testimonial.js
+++ src/testimonial/Testimonial.js
@@ -14,6 +14,7 @@ class Testimonial extends Component {
                     <div className="thumbnail">
                     </div>
                 </div>
+                <div className="decor"></div>
                 <div className="quote-box">
                     <div className="icons">
                         <img src={icons} className="icon-q" alt="icon" />
