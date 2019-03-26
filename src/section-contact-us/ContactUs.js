21822edf73608dd0d2601dccd018c311ec22c44a
OlehBabiy <luisowen11@gmail.com>
2019-03-26 18:53:38 +0200

    Added form.

--- src/section-contact-us/ContactUs.js
+++ src/section-contact-us/ContactUs.js
@@ -9,7 +9,14 @@ const ContactUs = () => (
         <div className="bg-contact">
             <img src={bg} alt="bg" className="bg-con-img" />
         </div>
-        <Form />
+        <Form>
+            <div className="container-context-form">
+                <div className="tx-p">Contact Us</div>
+                <div className="desct-p">Innovative solutions with the best.  Incididunt dolor sit amet,
+                    consectetur adipiscing elitsed tempor  vel metus scelerisque ante sollicitudin.
+            </div>
+            </div>
+        </Form>
     </div>
 );
 
--- src/section-contact-us/form/Form.js
+++ src/section-contact-us/form/Form.js
@@ -1,10 +1,52 @@
 import React from 'react';
 import './form.css';
 
-const Form = () => (
+const Form = (props) => (
 
     <div className="form-container">
-     </div>
+        {props.children}
+        <form>
+            <div className="wrap-name">
+                <div className="name">
+                    <input
+                        type="text"
+                        className=""
+                        placeholder="Name"
+                        name="Name"
+                    ></input>
+                </div>
+                <div className="email">
+                    <input
+                        type="email"
+                        className=""
+                        placeholder="Email"
+                        name="email"
+                    ></input>
+                </div>
+            </div>
+            <div className="wrap-subject">
+                <input
+                    type="text"
+                    className="subject"
+                    placeholder="Subject"
+                    name="subject"
+                ></input>
+            </div>
+
+            <div className="wrap-message">
+                <input
+                    type="text"
+                    className="message"
+                    placeholder="Messege"
+                    name="messege"
+                ></input>
+            </div>
+
+            <div className="createAccount">
+                <button type="submit">Submit Now</button>
+            </div>
+        </form>
+    </div>
 
 );
 
