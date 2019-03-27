bbaf54f9d82a9f7ac60f1bcb4687f37a7e67894c
OlehBabiy <luisowen11@gmail.com>
2019-03-27 13:42:04 +0200

    Made form and added style.

--- src/section-contact-us/form/Form.js
+++ src/section-contact-us/form/Form.js
@@ -4,50 +4,48 @@ import './form.css';
 const Form = (props) => (
 
     <div className="form-container">
-        {props.children}
+        <div className="content-form">
+            {props.children}
+        </div>
         <form>
-            <div className="wrap-name">
-                <div className="name">
+            <div className="wrapper">
+                <div className="wrap-name">
+                    <div className="name">
+                        <input
+                            type="text"
+                            className=""
+                            placeholder="Your name"
+                            name="Name"
+                        ></input>
+                    </div>
+                    <div className="email">
+                        <input
+                            type="email"
+                            className=""
+                            placeholder="Your email"
+                            name="email"
+                        ></input>
+                    </div>
+                </div>
+                <div className="wrap-subject">
                     <input
                         type="text"
-                        className=""
-                        placeholder="Name"
-                        name="Name"
+                        className="subject"
+                        placeholder="Subject"
+                        name="subject"
                     ></input>
                 </div>
-                <div className="email">
-                    <input
-                        type="email"
-                        className=""
-                        placeholder="Email"
-                        name="email"
-                    ></input>
+
+                <div className="wrap-message">
+                    <textarea placeholder="Message" className="message"></textarea>
                 </div>
-            </div>
-            <div className="wrap-subject">
-                <input
-                    type="text"
-                    className="subject"
-                    placeholder="Subject"
-                    name="subject"
-                ></input>
-            </div>
 
-            <div className="wrap-message">
-                <input
-                    type="text"
-                    className="message"
-                    placeholder="Messege"
-                    name="messege"
-                ></input>
-            </div>
-
-            <div className="createAccount">
-                <button type="submit">Submit Now</button>
+                <div className="createAccount">
+                    <button type="submit">Submit Now</button>
+                </div>
             </div>
         </form>
     </div>
-
 );
 
 export default Form;
