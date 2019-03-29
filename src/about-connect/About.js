cd053212dcb02bd3e7b149feaf8a9790826d3363
OlehBabiy <luisowen11@gmail.com>
2019-03-29 16:50:05 +0200

    has increased the width wrap-content-ct.

--- src/about-connect/About.js
+++ src/about-connect/About.js
@@ -10,7 +10,7 @@ class Header extends Component {
                     <img src={img} alt="img" className="img-ab-ct" />
                 </div>
                 <div className="wrap-content-ct">
-                    <div className="ct-t">Best way to connect customers & lead them</div>
+                    <div className="ct-text">Best way to connect customers & lead them</div>
                     <div className="ct-desc">Innovative solutions with the best.
                      Incididunt dolor sit amet, consectetur adipiscing elit, sed tempor  sit amet nibh libero,
                      in gravida nulla. vel metus scelerisque ante sollicitudin. </div>
